b6c0c75ac75bb947bca20d2b4b3db7fb31f14fa6
gandlea <grandlea@students.zhaw.ch>
2026-02-25 11:29:00 +0100

    korr Name

--- views/home.py
+++ views/home.py
@@ -7,7 +7,7 @@ st.title("Meine erste Streamlit App")
 """
 Diese App wurde von folgenden Personen entwickelt:
 - Léa Grandchamp (grandlea@students.zhaw.ch)
-- Brigit Marxe (marxebri@students.zhaw.ch)
+- Brigit Marxer (marxebri@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
